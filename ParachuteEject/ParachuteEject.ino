b1e5b4f7e1c8691cf6c09249b8c872d775bd14d7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-02-26 09:55:53 +0100

    Capsule ejection system

--- ParachuteEject/ParachuteEject.ino
+++ ParachuteEject/ParachuteEject.ino
@@ -1,38 +1,54 @@
 #include <Servo.h> 
 
+// Model B450
+
+/** Pins for parachute ejection. */
 int paraSensorPin = 12;
 int paraServoPin = 11;
 
-// Reference to the Servo that ejects the parachute.
-Servo paraEjectServo;
+/** Pins for capsule ejection. */
+int capsuleSensorPin = 8;
+int capsuleServoPin = 9;
+
+/** Servos that either eject the capsule or the parachute. */
+Servo paraEjectServo, capsuleEjectServo;
 
 void setup() {
-  // Initialize the PWN input parachute sensor.
+  // Initialize the PWN input sensors.
   pinMode(paraSensorPin, INPUT);
+  pinMode(capsuleSensorPin, INPUT);
 
   // Attach the servo to the parachute servo pin.
   paraEjectServo.attach(paraServoPin);
+  capsuleEjectServo.attach(capsuleServoPin);
 
   // Enable logging at the specified serial port.
   Serial.begin(9600);    
 }
 
 void loop() {
+  
   if (checkParachuteEject()) {
-    Serial.print("Eject!");
-
+    Serial.print("Eject Parachute!\n");
     paraEjectServo.write(90);
   } else {
-    Serial.print("NO Eject!");
-
     paraEjectServo.write(0);
   }
-  Serial.print("\n");
+
+  if (checkCapsuleEject()) {
+    Serial.print("Eject Capsule\n");
+
+    // Eject Capsule Servo
+  } else {
+    
+  }
 }
 
-boolean checkParachuteEject() {
-  int pulse = pulseIn(paraSensorPin, HIGH);
-  
-  return pulse <= 1700 && pulse != 0;
+boolean checkCapsuleEject() {
+  return pulseIn(capsuleSensorPin, HIGH) > 1100;
+}
+
+boolean checkParachuteEject() {  
+  return pulseIn(paraSensorPin, HIGH) > 1700;
 }
 
