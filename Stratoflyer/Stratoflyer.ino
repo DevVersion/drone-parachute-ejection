55a3125eaddf6f655f0facc5d00190c6e69ed535
Paul Gschwendtner <paulgschwendtner@gmail.com>
2018-02-26 15:46:13 +0100

    Add auto hover toggle

--- Stratoflyer/Stratoflyer.ino
+++ Stratoflyer/Stratoflyer.ino
@@ -1,22 +1,32 @@
 #include <Servo.h>
 
+#define AH_NEUTRAL 0
+#define AH_ENABLED 1
+#define AH_DISABLED 2
+
 // Model B450
 
 /** Pins for parachute ejection. */
-int paraSensorPin = 12;
+int paraSensorPin = 10;
 int paraServoPin = 11;
 
 /** Pins for capsule ejection. */
 int capsuleSensorPin = 8;
 int capsuleServoPin = 9;
 
+/** Pins for enabling the auto-hover */
+int autoHoverSensorPin = 12;
+
 /** Servos that either eject the capsule or the parachute. */
 Servo paraEjectServo, capsuleEjectServo;
+
+bool autoHover = false;
 
 void setup() {
   // Initialize the PWN input sensors.
   pinMode(paraSensorPin, INPUT);
   pinMode(capsuleSensorPin, INPUT);
+  pinMode(autoHoverSensorPin, INPUT);
 
   // Attach the servo to the parachute servo pin.
   paraEjectServo.attach(paraServoPin);
@@ -24,8 +34,6 @@ void setup() {
 
   // Enable logging at the specified serial port.
   Serial.begin(9600);
-
-  
 }
 
 void loop() {
@@ -40,16 +48,47 @@ void loop() {
   if (checkCapsuleDisconnect()) {
     Serial.print("Disconnect Capsule\n");
   }
+
+  determineAutoHoverState();
+}
+
+/**
+ * Logic & behavior
+ */
+
+void determineAutoHoverState() {
+  int autoHoverState = getAutoHoverState();
+
+  if (autoHoverState == AH_ENABLED  && !autoHover) {
+    autoHover = true;
+    Serial.print("Auto Hover enabled!\n");
+  } else if (autoHoverState == AH_DISABLED && autoHover) {
+    autoHover = false;
+    Serial.print("Auto Hover disabled!\n");
+  }
 }
 
 /**
  * Pin check functions
  */
 
-boolean checkCapsuleDisconnect() {
+bool checkCapsuleDisconnect() {
   return pulseIn(capsuleSensorPin, HIGH) > 1100;
 }
 
-boolean checkParachuteEject() {  
+bool checkParachuteEject() {
   return pulseIn(paraSensorPin, HIGH) > 1700;
 }
+
+int getAutoHoverState() {
+  int pulse = pulseIn(autoHoverSensorPin, HIGH) - 1500;
+
+  if (pulse > 50) {
+    return AH_ENABLED;
+  } else if (pulse < -50) {
+    return AH_DISABLED;
+  }
+
+  return AH_NEUTRAL;
+}
+
